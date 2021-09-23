e001dafcda8e5f6a4397c7aa87bb65e9e1570647
jerrywithaz <dev@jerrywithaz.com>
2021-09-23 06:54:01 -0500

    rebuild

--- dist/ant-design/Page/Page.js
+++ dist/ant-design/Page/Page.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const antd_1 = require("antd");
-const Layout_1 = require("../Layout");
 const { Title, Paragraph } = antd_1.Typography;
-const Page = ({ doc, projectName }) => {
-    return ((0, jsx_runtime_1.jsxs)(Layout_1.default, Object.assign({ projectName: projectName }, { children: [(0, jsx_runtime_1.jsx)(Title, { children: doc.name }, void 0), (0, jsx_runtime_1.jsx)(Paragraph, { children: doc.jsDoc.description }, void 0)] }), void 0));
+const Page = ({ doc }) => {
+    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)(Title, { children: doc.name }, void 0), (0, jsx_runtime_1.jsx)(Paragraph, { children: doc.jsDoc.description }, void 0)] }, void 0));
 };
 exports.default = Page;
 //# sourceMappingURL=Page.js.map
