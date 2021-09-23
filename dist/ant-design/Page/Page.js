87bc7e78829cb32fd15487237b61e4be4fbf3113
jerrywithaz <dev@jerrywithaz.com>
2021-09-23 12:24:44 -0500

    update

--- dist/ant-design/Page/Page.js
+++ dist/ant-design/Page/Page.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const antd_1 = require("antd");
+const Properties_1 = require("../Properties");
 const { Title, Paragraph } = antd_1.Typography;
 const Page = ({ doc }) => {
-    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)(Title, { children: doc.name }, void 0), (0, jsx_runtime_1.jsx)(Paragraph, { children: doc.jsDoc.description }, void 0)] }, void 0));
+    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)(Title, { children: doc.name }, void 0), (0, jsx_runtime_1.jsx)(Paragraph, { children: doc.jsDoc.description }, void 0), (0, jsx_runtime_1.jsx)(Properties_1.default, { properties: doc.properties }, void 0)] }, void 0));
 };
 exports.default = Page;
 //# sourceMappingURL=Page.js.map
